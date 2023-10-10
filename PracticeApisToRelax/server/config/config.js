2faedda2e41ed99409360d675a765a64ab0349aa
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-10-10 11:12:05 -0400

    cleaning

--- PracticeApisToRelax/server/config/config.js
+++ PracticeApisToRelax/server/config/config.js
@@ -2,26 +2,25 @@
 require('dotenv').config({ path: "../.env" });
 module.exports ={
   "development": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: "disadmin",
+    password: 'BA£[5p0ir48@',
+    database: 'cruditdb',
+    host: 'localhost',
+    dialect: mysql
   },
   "test": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "production": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-   
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
 }
 
