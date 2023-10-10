4efafe391cf85c6f7bb7fe85691b2a6ae88dbba3
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-10 12:56:16 -0400

    Update App.js
    Pushing to test/debug

--- PracticeApisToRelax/client/src/App.js
+++ PracticeApisToRelax/client/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes, Link, Switch } from 'react-router-dom';
 import React, { useEffect, useState, createContext } from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Login from './pages/login';
+
+// The /pages/login will be imported into the modal
+// import Login from './pages/login';
 import './App.css';
 import Register from './pages/register';
 
@@ -9,36 +11,19 @@ import Register from './pages/register';
 
 function App() {
 
-
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <div className="App">
-
-
-
-
-
-
         <Router>
           <Routes>
-
-
-            <Route path="/" exact Component={Login} />
-
-
-
-            <Route path="/login" exact Component={Login} />
-            <Route path="/register" exact Component={Register} />
-
+            {/* <Route path="/" exact Component={Login} /> */}
+            {/* <Route path="/login" exact Component={Login} /> */}
+            {/* <Route path="/register" exact Component={Register} /> */}
           </Routes>
         </Router>
-
-
-
-
       </div>
     </div>
   );
