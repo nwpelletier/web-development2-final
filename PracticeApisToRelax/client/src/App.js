3ce025a3deb8946e152ea827ecfe438ddee14143
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-10 10:44:54 -0400

    Front End Update
    Components (navbar, which includes SubCrudditList, and LowerHalf)
    -imported into app.js
    -CSS to go along with it
    -index.html includes bootstrap scripts

--- PracticeApisToRelax/client/src/App.js
+++ PracticeApisToRelax/client/src/App.js
@@ -1,5 +1,6 @@
-import {BrowserRouter as Router, Route, Routes, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Switch } from 'react-router-dom';
 import React, { useEffect, useState, createContext } from 'react';
+import Navbar from './components/Navbar/Navbar';
 import Login from './pages/login';
 import './App.css';
 import Register from './pages/register';
@@ -10,30 +11,35 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div className="App">
 
 
-      
 
 
-      
-      <Router>
-        <Routes>
 
-        
-          <Route path="/" exact Component={Login}/>
-        
-      
 
-          <Route path="/login" exact Component={Login}/>
-          <Route path="/register" exact Component={Register}/>
-          
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
 
- 
-     
 
+            <Route path="/" exact Component={Login} />
+
+
+
+            <Route path="/login" exact Component={Login} />
+            <Route path="/register" exact Component={Register} />
+
+          </Routes>
+        </Router>
+
+
+
+
+      </div>
     </div>
   );
 }
