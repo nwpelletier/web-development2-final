a0c1a7cba40bc008fa2df959176302223fadedd2
Alex Franklin <alexandra.a.franklin@gmail.com>
2023-10-11 15:15:35 -0400

    fixed issue with votes

--- PracticeApisToRelax/server/controllers/votes-controller.js
+++ PracticeApisToRelax/server/controllers/votes-controller.js
@@ -5,74 +5,84 @@ const validator = require('validator');
 module.exports = {
     // TODO: add transactions
     // TODO: modify commented when auth is included
-    addVote : async (req, res)=> {
-        const vote = req.body
-        const postId = req.params.id
-       // const userId = req.UserId
-       const userId = vote.UserId;
+    addVote: async (req, res) => {
+        const vote = req.body;
+        const postId = req.params.id;
+        const userId = vote.UserId;
+        // const userId = req.UserId
+    
         try {
+            if (vote.liked === undefined) {
+                return res.status(400).json({ message: "Vote cannot be null" });
+            }
+    
             let points = 0;
             const post = await Posts.findOne({
                 where: {
                     id: postId
                 }
-            })
-            if (!post){
-                res.status(404).json({message: "That post does not exist"})
+            });
+    
+            if (!post) {
+                return res.status(404).json({ message: "That post does not exist" });
             }
+    
             const voteExists = await Votes.findOne({
                 where: {
                     UserId: userId,
                     PostId: postId
                 }
-            })
-            if (voteExists){
-                console.log("hellow?")
+            });
+    
+            if (voteExists) {
+                console.log("hello?");
                 if (voteExists.liked === vote.liked) {
-                    return res.status(400).json({message: "Cannot vote same way more than once"})
+                    return res.status(400).json({ message: "Cannot vote the same way more than once" });
                 }
+    
                 if (vote.liked) {
                     points = 2;
                 } else {
                     points = -2;
                 }
                 voteExists.liked = vote.liked;
-                voteExists.save()
-                .then(()=> {
-                }).catch((error) => {
-                    return res.status(500).json({message: "Could not complete voting", error: error})
-                })
+    
+                try {
+                    await voteExists.save();
+                } catch (error) {
+                    return res.status(500).json({ message: "Could not complete voting", error: error });
+                }
             } else {
-                if (vote.liked){
+                if (vote.liked) {
                     points = 1;
                 } else {
                     points = -1;
                 }
                 vote.UserId = userId;
-                vote.PostId = postId
-                Votes.create(vote)
-                .then(()=> {
-                }).catch((error) => {
-                    return res.status(500).json({message: "Could not complete voting", error: error})
-                })
+                vote.PostId = postId;
+    
+                try {
+                    await Votes.create(vote);
+                } catch (error) {
+                    return res.status(500).json({ message: "Could not complete voting", error: error });
+                }
             }
-
+    
             post.points += points;
-            post.save()
-            .then(()=> {
-                return res.status(200).send({message: "successfully vote", post: post})
-            }).catch((error)=> {
-                return res.status(500).send({message: "Could not complete voting", error: error})
-            })
-            
-            
-
-
-        } catch(error) {
+    
+            try {
+                await post.save();
+                res.status(200).json({ message: "Successfully voted", post: post });
+            } catch (error) {
+                return res.status(500).json({ message: "Could not complete voting", error: error });
+            }
+    
+        } catch (error) {
             console.log(error);
-           return res.status(500).json({message: "Internal Server Error"}) 
+            res.status(500).json({ message: "Internal Server Error" });
         }
     },
+    
     deleteVote: async (req, res) => {
         const userId = req.body.UserId
         const postId = req.params.id
