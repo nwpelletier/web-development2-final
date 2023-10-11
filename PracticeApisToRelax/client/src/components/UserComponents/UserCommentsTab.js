12e883941be6cc0639103c38d697b3874b674888
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-10-11 12:41:51 -0400

    user page comments display

--- PracticeApisToRelax/client/src/components/UserComponents/UserCommentsTab.js
+++ PracticeApisToRelax/client/src/components/UserComponents/UserCommentsTab.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
 
 function UserCommentsTab() {
-  // Sample comments data
+  let { id } = useParams();
+  const [comments, setComments] = useState([]);
+  id = 1;
+  useEffect(() => {
+    try {
+      axios
+        .get(`http://localhost:8080/api/posts/${id}/comments`)
+        .then((response) => {
+          setComments(response.data);
+        });
+    } catch (error) {
+      
+    }
+  },[]);
+
+  // Sample comments data to test
   const userComments = [
     {
       text: "This is the first comment.",
@@ -9,16 +26,18 @@ function UserCommentsTab() {
     {
       text: "Another comment by the user.",
     },
-    // Add more comments here
-  ];
+      ];
 
   return (
     <div className="container mt-3">
       <ul className="list-group">
-        {userComments.map((comment, index) => (
+        {comments.map((comment, index) => (
           <li key={index} className="list-group-item col-md-8 mb-3 border rounded p-3 shadow">
             <div className="">
-              {comment.text}
+              {comment.title}
+            </div>
+            <div className="">
+              {comment.caption}
             </div>
           </li>
         ))}
