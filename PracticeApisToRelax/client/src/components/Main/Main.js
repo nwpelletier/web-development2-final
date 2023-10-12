e2bc05a6e8436146cc3adacf6ecfc9dc6cf012df
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-11 21:07:58 -0400

    Posts appear on root page
    They reflect the database info.
    -CSS to go with it
    -have to change date format

--- PracticeApisToRelax/client/src/components/Main/Main.js
+++ PracticeApisToRelax/client/src/components/Main/Main.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import Post from '../Post/Post'
-import DemoPost from '../Post/DemoPost'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Post from '../Post/DemoPost';
 
 function Main() {
+
+  // Default: will display posts by new
+  // Todo: implement useEffect to allow toggling between hot/new
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/posts/posts/new')
+      .then(response => {
+        setPosts(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []); 
+
   return (
     <div>
-      <DemoPost/><DemoPost/><DemoPost/><DemoPost/><DemoPost/><DemoPost/><DemoPost/><DemoPost/>
+      {posts.map(posts => (
+        <Post
+          key={posts.id}
+          points={posts.points}
+          title={posts.title}
+          postType={posts.postType}
+          UserId={posts.UserId}
+          SubcrudditId={posts.SubcrudditId}
+          createdAt={posts.createdAt}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- PracticeApisToRelax/client/src/components/Post/DemoPost.js
+++ PracticeApisToRelax/client/src/components/Post/DemoPost.js
@@ -2,9 +2,17 @@ import React from 'react';
 import arrowUpImage from "../../assets/arrow-square-up-svgrepo-com.svg";
 import arrowDownImage from "../../assets/arrow-square-down-svgrepo-com.svg";
 
-// Formatting it, then will incorporate "get" request
+function Post({ points, title, postType, UserId, SubcrudditId, createdAt }) {
+  console.log("Request received with the following data:");
+  console.log("points:", points);
+  console.log("title:", title);
+  console.log("postType:", postType);
+  console.log("userId:", UserId);
+  console.log("subcrudditId:", SubcrudditId);
+  console.log("createdAt:", createdAt);
 
-function Post() {
+
+
   return (
     <div>
       <div className="post-container row">
@@ -17,8 +25,7 @@ function Post() {
               width="40%"
               height="40%">
             </img>
-            {/*  */}
-            <h6 className="vote-count">3456</h6>
+            <h6 className="vote-count">{points}</h6>
             <img
               className="downvote"
               src={arrowDownImage}
@@ -28,11 +35,19 @@ function Post() {
             </img>
           </div>
           <div className="post-type-container">
-            <p>post type</p>
+            <p>{postType}</p>
           </div>
         </div>
 
         <div className="post-content-container col-md-10 col-sm-5 row">
+          <div className="post-title">{title}</div>
+          <div className="post-submission-info">Posted {createdAt} by {UserId} to {SubcrudditId}</div>
+          <div className="post-links">
+            <span># of child comments</span>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span>report</span>
+          </div>
+
         </div>
       </div>
     </div>
