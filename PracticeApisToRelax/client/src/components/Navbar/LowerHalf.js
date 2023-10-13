55453678258ea92271ce4c99682b03ef3803f54a
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-12 20:40:46 -0400

    Welcome message upon login
    It shows up in nav, same spot as "login or sign up".
    
    Formatting is ass right now, will work on it after; but am going then make that an href to the userpage

--- PracticeApisToRelax/client/src/components/Navbar/LowerHalf.js
+++ PracticeApisToRelax/client/src/components/Navbar/LowerHalf.js
@@ -1,12 +1,19 @@
-// 2023/10/10 Calling it "lower half" because that's all it is now - might rename to something that makes more sense
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../Modal/Modal';
 
 function LowerHalf() {
   const [modalContent, setModalContent] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Content will either be 'login' or 'signup' for this modal
+  useEffect(() => {
+    // Check for the presence of a JWT token in local storage
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Token found, consider the user authenticated
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   const openModal = (content) => {
     setModalContent(content);
   };
@@ -14,28 +21,34 @@ function LowerHalf() {
   return (
     <div className="lower-half">
       <div className="login-box">
-        Want to join?{' '}
-        <span
-          id="link"
-          onClick={() => openModal('login')}
-          data-bs-toggle="modal"
-          data-bs-target="#defaultModal"
-        >
-          Log in
-        </span>{' '}
-        or{' '}
-        <span
-          id="link"
-          onClick={() => openModal('signup')}
-          data-bs-toggle="modal"
-          data-bs-target="#defaultModal"
-        >
-          sign up
-        </span>{' '}
-        in seconds. |
+        {isAuthenticated ? (
+          <p>Welcome, {localStorage.getItem('username')}!</p>
+        ) : (
+          <span>
+            Want to join?{' '}
+            <span
+              id="link"
+              onClick={() => openModal('login')}
+              data-bs-toggle="modal"
+              data-bs-target="#defaultModal"
+            >
+              Log in
+            </span>{' '}
+            or{' '}
+            <span
+              id="link"
+              onClick={() => openModal('signup')}
+              data-bs-toggle="modal"
+              data-bs-target="#defaultModal"
+            >
+              sign up
+            </span>{' '}
+            in seconds. |
+          </span>
+        )}
       </div>
-      {/* Create modal, pass content, and the ability to setModalContent */}
-      <Modal content={modalContent} setModalContent={setModalContent}/>
+
+      <Modal content={modalContent} setModalContent={setModalContent} />
     </div>
   );
 }
