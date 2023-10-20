5d52c6478d00298de6b75b7af10be0a306c3117b
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-20 00:55:26 -0400

    "Pagination for dummies" - ugly, but functional
    Went through Jing's demonstration for pagination!  Adapted what I could just to get some functionality down, but I don't have any proper classNames for it so it's looking hella default; I'll look more into properly formatting it afterwards ;)

--- PracticeApisToRelax/client/src/components/Main/SubcrudditDisplay.js
+++ PracticeApisToRelax/client/src/components/Main/SubcrudditDisplay.js
@@ -1,27 +1,55 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Post from '../Post/Post';
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import ReactPaginate from 'react-paginate';
 import { formatDistance } from 'date-fns';
+import Post from '../Post/Post';
+
 import { ModContext } from '../../pages/Subcruddit';
+import { btnText } from '../CreatePost/textPostInit';
 export const SubcrudditContext = createContext();
+
+
 function SubcrudditDisplay({ subcrudditName, sortingType }) {
 
+  // Testing paginate
   const [posts, setPosts] = useState([]);
-  const [isMod, setIsMod] = useContext(ModContext);
+  const [pageNumber, setPageNumber] = useState(0);
+  const postsPerPage = 5; // Renamed to avoid conflict with the 'posts' state array
+  const offset = pageNumber * postsPerPage;
+  const displayPosts = posts
+    .slice(offset, offset + postsPerPage)
+    .map((post) => (
+      <Post
+        key={post.id}
+        id={post.id}
+        points={post.points}
+        title={post.title}
+        postType={post.postType}
+        username={post.username}
+        SubcrudditName={post.SubcrudditName}
+        createdAt={formatDistance(new Date(post.createdAt), new Date(), {
+          addSuffix: true,
+        })}
+      />
+    ));
+  const pageCount = Math.ceil(posts.length / postsPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  }
 
+
+  // Verifying mod status!
+  const [isMod, setIsMod] = useContext(ModContext);
+  console.log('Subcruddit Mod Status: ' + isMod);
+  const [sortBy, setSortBy] = useState('')
+
+  // Default sorting type!
   if (!sortingType) {
     sortingType = "hot";
   }
 
-  // Verifying mod status!
-  console.log('Subcruddit Mod Status: ' + isMod);
 
-
-  const [sortBy, setSortBy] = useState('')
-
-
-
+  // Get posts by sorting type!
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -44,24 +72,25 @@ function SubcrudditDisplay({ subcrudditName, sortingType }) {
     fetchPosts();
   }, [subcrudditName, sortingType]);
 
+
+  // Display
   return (
     <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          points={post.points}
-          title={post.title}
-          postType={post.postType}
-          username={post.username}
-          SubcrudditName={post.SubcrudditName}
-          createdAt={formatDistance(new Date(post.createdAt), new Date(), {
-            addSuffix: true,
-          })}
-        />
-      ))}
+      {displayPosts}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={btnText}
+        previousLinkClassName={btnText}
+        nextLinkClassName={btnText}
+        disabledClassName={btnText}
+        activeClassName={btnText}
+      />
     </div>
   );
+
 
 }
 
---
+++ PracticeApisToRelax/client/src/components/Main/SubcrudditDisplay4.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Post from '../Post/Post';
+import { formatDistance } from 'date-fns';
+import { ModContext } from '../../pages/Subcruddit';
+export const SubcrudditContext = createContext();
+function SubcrudditDisplay({ subcrudditName, sortingType }) {
+
+  const [posts, setPosts] = useState([]);
+  const [isMod, setIsMod] = useContext(ModContext);
+
+  if (!sortingType) {
+    sortingType = "hot";
+  }
+
+  // Verifying mod status!
+  console.log('Subcruddit Mod Status: ' + isMod);
+
+
+  const [sortBy, setSortBy] = useState('')
+
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        let response;
+        if (subcrudditName === 'all') {
+          response = await axios.get(
+            `http://localhost:8080/api/posts/posts/${sortingType}`
+          );
+        } else {
+          response = await axios.get(
+            `http://localhost:8080/api/posts/posts/${subcrudditName}/${sortingType}`
+          );
+        }
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, [subcrudditName, sortingType]);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          points={post.points}
+          title={post.title}
+          postType={post.postType}
+          username={post.username}
+          SubcrudditName={post.SubcrudditName}
+          createdAt={formatDistance(new Date(post.createdAt), new Date(), {
+            addSuffix: true,
+          })}
+        />
+      ))}
+    </div>
+  );
+
+}
+
+export default SubcrudditDisplay;
