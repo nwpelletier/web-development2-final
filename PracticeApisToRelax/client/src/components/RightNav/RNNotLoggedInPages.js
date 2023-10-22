3b63ab68eb5fc4e3e93da9152c852aef80e9f240
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-10-22 13:13:53 -0400

    right nav wiki; still working on it

--- PracticeApisToRelax/client/src/components/RightNav/RNNotLoggedInPages.js
+++ PracticeApisToRelax/client/src/components/RightNav/RNNotLoggedInPages.js
@@ -10,9 +10,11 @@ import { BASE_API_URL } from "../../utils/constant";
 function RNNotLoggedInPages(margin) {
   const [isMod, setIsMod] = useContext(ModContext);
   const [usernames, setUsernames] = useState([]);
+  const [subcrudWiki, setSubcrudWiki] = useState("");
   const [subcrudName, setSubcrudName] = useContext(SubCrudditContext);
   const location = useLocation();
   const navigate = useNavigate();
+  const welcomeText = subcrudName === 'all' ? 'Welcome to cruddit!  Feel free to explore the many subcruddits we offer!  We welcome all types of communities, and are sure you will enjoy your stay :)' : `Welcome to /c/${subcrudName}`;
 
   // Get list of mods! (Axios + filter by subcrudName)
   useEffect(() => {
@@ -35,6 +37,18 @@ function RNNotLoggedInPages(margin) {
     getModList();
   }, []);
 
+  useEffect(() => {
+    const getWiki = async () => {
+      try {
+        const wikiResponse = await axios.get(`${BASE_API_URL}/api/subcruddits/${subcrudName}`);
+        setSubcrudWiki(wikiResponse.data.subcruddit.wiki);
+      } catch (error) {
+        console.error('Error retrieving wiki', error);
+      }
+    };
+    getWiki(); // Call the getWiki function here
+  }, [subcrudName]);
+
 
   //console.log("RIGHTNAV POSITION: ",margin.margin)
   const currentPath = useLocation().pathname.split("/")[2];
@@ -51,42 +65,55 @@ function RNNotLoggedInPages(margin) {
   return (
     <>
       <div id="right-panel-rnnot" style={{ marginTop: margin.margin }}>
+        <p>
+          <strong className="right-nav-subcruddit-name"> /c/{currentPath} </strong>
+        </p>
         <div className="text-center right-nav-create-submit mt-3">
           <p className="mt-3 " onClick={() => navigate(`/post/text/${subcrudName}`)}>
             Submit a post
           </p>
         </div>
         {(location.pathname.includes('/c/all') || location.pathname.includes('/userpage')) && (
-        <div className="text-center right-nav-create-submit mt-3">
-          <p className="mt-3 " onClick={() => navigate(`/c/new`)}>
-            Create a subcruddit
-          </p>
-        </div>
+          <div className="text-center right-nav-create-submit mt-3">
+            <p className="mt-3 " onClick={() => navigate(`/c/new`)}>
+              Create a subcruddit
+            </p>
+          </div>
         )}
-        <p>
-          <strong className="right-nav-subcruddit-name"> /c/{currentPath} </strong>
-        </p>
-        <p><small className="text-danger">Please read our rules in their entirety before participating.</small></p>
-        <div className="rules-box">
-          {rules.map((rule, index) => (
-            <ul className="right-nav-rules" key={index}>
-              <div>{rule}</div>
-            </ul>
-          ))}
-        </div>
-        <div className="right-nav-mod-list align-items-center">
-          {usernames.length > 0 && <p>====MODERATORS====</p>}
-          {usernames.length > 0 && (
-            <div className="right-nav-mod-container col-5">
-              <ul>
-                {usernames.map((username, index) => (
-                  <li key={index}>{username}</li>
-                ))}
-              </ul>
-
+        <div className="rules-container">
+          <details><summary className="right-nav-rules-dropdown">click to read our rules</summary>
+            <div className="rules-box">
+              {rules.map((rule, index) => (
+                <ul className="right-nav-rules" key={index}>
+                  <div>{rule}</div>
+                </ul>
+              ))}
             </div>
+          </details>
+        </div>
+        <div className="moderator-container">
+          <div className="right-nav-mod-list align-items-center">
+            {usernames.length > 0 && <p>====MODERATORS====</p>}
+            {usernames.length > 0 && (
+              <div className="col-5">
+                <ul>
+                  {usernames.map((username, index) => (
+                    <li key={index}>{username}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            {usernames.length > 0 && <p>==================</p>}
+          </div>
+        </div>
+        <div className="wiki-container">
+          <span className="wiki-title">/c/{subcrudName} wiki</span>
+          <hr></hr>
+          {subcrudWiki ? (
+            subcrudWiki
+          ) : (
+            <p>{welcomeText}</p>
           )}
-          {usernames.length > 0 && <p>==================</p>}
         </div>
       </div>
     </>
