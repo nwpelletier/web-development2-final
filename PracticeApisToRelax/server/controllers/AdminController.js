55382894eb4201733783de8d6ba3ffba015b89a7
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-10-22 14:22:17 -0400

    admin page

--- PracticeApisToRelax/server/controllers/AdminController.js
+++ PracticeApisToRelax/server/controllers/AdminController.js
@@ -9,19 +9,31 @@ exports.findAllUsers = async (req, res) => {
   try {
     const users = await Users.findAll();
 
-
-      const usersObj = users.map((user) => ({
-        
+    const usersObj = users.map((user) => ({
       id: user.id,
       username: user.username,
       email: user.email,
       role: user.role,
       isActive: user.isActive,
-      
     }));
     res.status(200).send(usersObj);
   } catch (error) {
     //console.log("ERROR: ", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
+};
+
+exports.toggleActive = async (req, res) => {
+console.log("ADMIN CONTROLLER BODY",req.params)
+
+  const user = await Users.findByPk(req.params.id);
+
+  if (!user) return res.status(404).send("User Not Found");
+
+  user.isActive = !user.isActive;
+
+  user.save()
+    .then(() => {
+      return res.status(200).send({id: user.id, isActive: user.isActive});
+  })
 };
